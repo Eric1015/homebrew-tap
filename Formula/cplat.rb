ed60030fb46e7d68e2aa82de23f7e4119bb36cb2
Eric1015 <love1dbsb77@hotmail.com>
2023-09-04 22:44:17 -0700

    Fixed the missing bump up in url

--- Formula/cplat.rb
+++ Formula/cplat.rb
@@ -1,7 +1,7 @@
 class Cplat < Formula
   desc "CLI for parsing Cplat (copy latest)"
   homepage "https://github.com/Eric1015/cplat"
-  url "https://github.com/Eric1015/cplat/releases/download/0.1.0/cplat-0.1.0-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/Eric1015/cplat/releases/download/0.1.1/cplat-0.1.1-x86_64-apple-darwin.tar.gz"
   sha256 "f5ae6c00b8bd602c4b1ad9113f3d0c53e05532020f9049c10c1144b8b3aa183d"
   version "0.1.1"
 
