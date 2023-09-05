c73f924d1859f45d19408bf9403637567c4f018c
Eric1015 <love1dbsb77@hotmail.com>
2023-09-04 22:22:31 -0700

    Updated the sha256

--- Formula/cplat.rb
+++ Formula/cplat.rb
@@ -2,7 +2,7 @@ class Cplat < Formula
   desc "CLI for parsing Cplat (copy latest)"
   homepage "https://github.com/Eric1015/cplat"
   url "https://github.com/Eric1015/cplat/releases/download/0.1.0/cplat-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "94aa47e282f5eaaf8b2779b122643e6ab58f90a8"
+  sha256 "f6e7bbd84c07e3e5c3d19bdcf5fd8f3a2fd4e1b04197d934afd335b3d672fd6e"
   version "0.1.0"
 
   def install
