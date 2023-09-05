f960e9a4f1ef054b0275b4cda8b2a1fd1d232472
Eric1015 <love1dbsb77@hotmail.com>
2023-09-04 22:34:14 -0700

    Bumped up to version 0.1.1

--- Formula/cplat.rb
+++ Formula/cplat.rb
@@ -2,8 +2,8 @@ class Cplat < Formula
   desc "CLI for parsing Cplat (copy latest)"
   homepage "https://github.com/Eric1015/cplat"
   url "https://github.com/Eric1015/cplat/releases/download/0.1.0/cplat-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "f6e7bbd84c07e3e5c3d19bdcf5fd8f3a2fd4e1b04197d934afd335b3d672fd6e"
-  version "0.1.0"
+  sha256 "f5ae6c00b8bd602c4b1ad9113f3d0c53e05532020f9049c10c1144b8b3aa183d"
+  version "0.1.1"
 
   def install
     bin.install "cplat"
